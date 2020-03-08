5b2b7b3ededf7fe8cb227e321f8062d8893a41e5
anil <anilkrishna@gmail.com>
2020-03-08 10:37:33 +0530

    addded buttons

--- src/app/mybattleship/myships/myships.component.ts
+++ src/app/mybattleship/myships/myships.component.ts
@@ -10,6 +10,7 @@ export class MyshipsComponent {
   grids: Grid[][];
   columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  ship ='';
   constructor() {
     this.gridInit();
   }
@@ -26,6 +27,37 @@ export class MyshipsComponent {
 
   onDestroyerClick() {
     console.log("on Destroyer clicked");
+    this.ship = 'destroyer';
   }
+
+  onCruiserClick() {
+
+    console.log("on Crusier clicked");
+    this.ship = 'cruiser';
+  }
+
+  onSubmarineClick() {
+    console.log("on Submarine clicked");
+    this.ship = 'sub'
+  }
+
+  onBattleshipClick() {
+    console.log("on Battleship clicked");
+    this.ship = 'battle';
+  }
+
+  onCarrierClick() {
+    console.log("on Carrier clicked");
+    this.ship ='carrier';
+  }
+
+  onCellClick(x, y) {
+    console.log(x,y);
+    console.log(this.grids[x][y]);
+
+  }
+
+
+
 
 }
